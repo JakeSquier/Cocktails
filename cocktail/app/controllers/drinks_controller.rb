ad8993af56eb3150c9adf8d1f6ad1f7007df3865
tomtom-code <tomlai8882000@hotmail.com>
2020-09-30 16:15:39 -0500

    update controller

--- cocktail/app/controllers/drinks_controller.rb
+++ cocktail/app/controllers/drinks_controller.rb
@@ -1,11 +1,13 @@
 class DrinksController < ApplicationController
 
     def index 
+        @drinks = Drink.all
 
     end 
 
     def show
-        
+        @drink = Drink.find(params[:id])
+
     end
 
 end
